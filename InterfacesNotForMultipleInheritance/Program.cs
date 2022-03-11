de804b3ffca72f5f8ef95e2c81eb1f195a5e2ac2
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-11 14:46:05 -0600

    added notes

--- InterfacesNotForMultipleInheritance/Program.cs
+++ InterfacesNotForMultipleInheritance/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+/*
+ 
+Topic:
+
+All we are doing is confirming to a contract we're making sure that the public interface of our class
+
+has the methods or members it has declared in the interface.
+ */
