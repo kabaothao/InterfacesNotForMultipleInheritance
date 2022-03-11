5536e00a2c78c598c41ca966a35d496f24550b69
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-11 14:46:42 -0600

    added more notes

--- InterfacesNotForMultipleInheritance/Program.cs
+++ InterfacesNotForMultipleInheritance/Program.cs
@@ -2,6 +2,29 @@
 Console.WriteLine("Hello, World!");
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
  
 Topic:
@@ -9,4 +32,15 @@ Topic:
 All we are doing is confirming to a contract we're making sure that the public interface of our class
 
 has the methods or members it has declared in the interface.
+
+
+
+
+Interfaces are used for building loosely coupled extensible and testable applications not for code reusability.
+
+What interfaces do provide polymorphic behavior and that's what I'm going explain to you in the next
+
+lecture.
+
+
  */
