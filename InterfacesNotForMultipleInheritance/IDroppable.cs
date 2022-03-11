045e59fff6d3ba40ff2a2cb68920a4438a060014
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-11 14:45:19 -0600

    updated notes

--- InterfacesNotForMultipleInheritance/IDroppable.cs
+++ InterfacesNotForMultipleInheritance/IDroppable.cs
@@ -11,3 +11,12 @@ namespace InterfacesNotForMultipleInheritance
         void Drop();
     }
 }
+
+/*
+ 
+It's fundamentally wrong to say a class inherits from an interface, a class implements an interface because
+
+an interface as you see here is simply a contract. It's saying that any class that implements this interface should have a method that is public void and called drop.
+
+And that's what we type here. So that's the truth about interfaces and inheritance.
+ */
